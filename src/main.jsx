e8b0a1b0d96b43198eb9b90406652f95cc51de55
Yamila_Perez <yamilaperez.dev@gmail.com>
2025-08-02 02:49:30 -0300

    retorno a cambio anterior

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import App from './App.jsx'
-import ContactContextProvider from './Context/ContactContext.jsx'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-    <ContactContextProvider>
-        <App/>
-    </ContactContextProvider>
+            <App />
     </BrowserRouter>
 )
